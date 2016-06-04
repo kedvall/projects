abc39b3a0d973a5ef6acd38f2533e7091b4b5f5c
Kedvall <kedvall123@gmail.com>
2016-06-04 03:49:33 -0500

    Added voltage reading to wireless testing

--- Wireless Testing.ino
+++ Wireless Testing.ino
@@ -9,13 +9,16 @@
 *                                                                     *
 **********************************************************************/
 //Defines
+#define LEDPIN 13 //Pin for built in LED
+#define MAXSAMPLES 10 //Number of samples to take each reading
 #define PRINTDELAY 1000 //Delay between sending H and L to toggle LED (in milliseconds)
 //Functionality Selection: Set 1 for transmitting mode, 0 for receiving mode
 #define MODESELECT 1 //Defualt is 1 (transmitting mode)
 
 //Variables
-const int ledPin = 13; //Pin for built in LED
 int serialData; //Variable to store incoming serial data
+int sample; //Current sample number 
+float sum; //Sum of current samples
 
 ///////////////////////////////////////////////////////////////////////
 // Initial Setup:                                                    //
@@ -28,7 +31,7 @@ void setup()
   Serial.begin(9600);
 
   if (!MODESELECT)
-    pinMode(ledPin, OUTPUT); //Set LED pin to output
+    pinMode(LEDPIN, OUTPUT); //Set LED pin to output
 } //End of setup function
 
 ///////////////////////////////////////////////////////////////////////
@@ -39,7 +42,8 @@ void setup()
 //     Check incoming Serial data for 'H' or 'L'. If either is       //
 //     received, toggle LED state respectively                       //
 ///////////////////////////////////////////////////////////////////////
-void loop() {
+void loop()
+{
   if (MODESELECT) //Mode is set to transmitting
   {
     Serial.print('H'); //Print H over serial (toggle LED on)
@@ -47,20 +51,52 @@ void loop() {
 
     Serial.print('L'); //Print L over serial (toggle LED off)
     delay(PRINTDELAY); //Wait again for specified delay
+
+    Serial.print(ReadVoltage());
+    delay(PRINTDELAY);
   }
   
   else //Mode is set to receiving
   {
     //Check for incoming serial data
-    if (Serial.available() > 0) {
+    if (Serial.available() > 0) 
+    {
       //Read oldest byte in the serial buffer and store it as int (ASCII is converted)
       serialData = Serial.read();
       //If it's an H, turn on the LED:
       if (serialData == 'H' || serialData == 'h')
-        digitalWrite(ledPin, HIGH);
+        digitalWrite(LEDPIN, HIGH);
       //If it's an L, turn off the LED:
-      if (serialData == 'L' || serialData == 'l')
-        digitalWrite(ledPin, LOW);
+      else if (serialData == 'L' || serialData == 'l')
+        digitalWrite(LEDPIN, LOW);
+      //If it's anything else (voltage reading, print it)
+      else
+      {
+        Serial.print('Voltage: ');
+        Serial.print(serialData);
+      }
     }
   }
 } //End of main loop
+
+///////////////////////////////////////////////////////////////////////
+// Function: ReadVoltage                                             //
+// Sample voltage readings of battery and return voltage             //
+///////////////////////////////////////////////////////////////////////
+float ReadVoltage()
+{
+  for (sample = 0; sample < MAXSAMPLES; sample++)
+  {
+    sum += analogRead(A2);
+    delay(10);
+  }
+  sum = 0;
+
+  return (sum / MAXSAMPLES);
+
+  /* 
+  Not sure the point of this code. You can add it back in if you need it
+  voltage = ((float)sum / (float)NUM_SAMPLES * 5.18) / 1024.0;
+  Serial.print(voltage * 5.99);
+  */
+}
