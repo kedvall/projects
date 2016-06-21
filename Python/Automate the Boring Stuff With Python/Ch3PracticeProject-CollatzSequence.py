e3cb46feca9560a76a79f3fbda9476dec3981b4e
Kedvall <kedvall123@gmail.com>
2016-06-21 08:31:43 -0500

    Finished chapter 3 practice program

--- Python/Automate the Boring Stuff With Python/Ch3PracticeProject-CollatzSequence.py
+++ Python/Automate the Boring Stuff With Python/Ch3PracticeProject-CollatzSequence.py
@@ -5,3 +5,17 @@ def collatz(number):
 	else:
 		print(3 * number + 1)
 		return 3 * number + 1
+
+# Use collatz sequence to eventually reach 1
+print('Enter a number:')
+try:
+	num = int(input())
+except ValueError:
+	print('Enter a integer:')
+	num = int(input())
+
+while num != 1:
+	try:
+		num = collatz(num)
+	except ValueError:
+		print('Please enter a number')
