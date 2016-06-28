146acebf8669c4cdacf7739a14fc6b50b6dbeac6
Kedvall <kedvall123@gmail.com>
2016-06-28 16:22:44 -0500

    Finished Password Locker project

--- Python/Automate the Boring Stuff With Python/ExampleBatchShortcut.bat
+++ Python/Automate the Boring Stuff With Python/ExampleBatchShortcut.bat
@@ -3,5 +3,8 @@ Rem Windows:	#! python3
 Rem OSX:		#! /usr/bin/env python3
 Rem Linux: 		#! /usr/bin/python3
 
-#! /usr/bin/python3
+Rem pyw is windowless, no execution window is displayed
+Rem %* forwards any command line arguements to the program
+
 @py.exe C:\path\to\your\pythonScript.py %*
+@pause
---
+++ Python/Automate the Boring Stuff With Python/Hello.bat
@@ -0,0 +1,2 @@
+@py.exe "C:\Users\intern\Documents\PracticeCode\Python\Automate the Boring Stuff With Python\Hello.py" %*
+@pause
