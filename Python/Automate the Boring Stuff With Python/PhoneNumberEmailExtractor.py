d4804c803c592a8688000c8ece1b39dd621d2d91
Kedvall <kedvall123@gmail.com>
2016-06-29 16:08:38 -0500

    Addex email regex

--- Python/Automate the Boring Stuff With Python/PhoneNumberEmailExtractor.py
+++ Python/Automate the Boring Stuff With Python/PhoneNumberEmailExtractor.py
@@ -11,6 +11,15 @@ phoneRegex = re.compile(r'''(
 	(\d{4})							# Last 4 digits
 	(\s*(ext|x|ext.)\s*(\d{2,5}))?	# Extension
 	)''', re.VERBOSE)
-# TODO: Create email regex
+
+emailRegex = re.compile(r'''(
+	[a-zA-z0-9._%+-]+		# Username
+	@						# @ symbol
+	[a-zA-Z0-9.-]+			# Domain name
+	(\.[a-zA-z]{2,4})		# Dot-something
+	)''', re.VERBOSE)
+
+
+
 # TODO: Find matches in clipbaord text
 # TODO: Copy results to the clipboard
