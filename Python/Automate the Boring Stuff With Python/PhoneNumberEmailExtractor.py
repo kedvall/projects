9f12ed7c1f241cc0471e1fb9b6c435d7e260aa8f
Kedvall <kedvall123@gmail.com>
2016-06-30 08:30:50 -0500

    Finished Phone Number and Email Address Extractor project

--- Python/Automate the Boring Stuff With Python/PhoneNumberEmailExtractor.py
+++ Python/Automate the Boring Stuff With Python/PhoneNumberEmailExtractor.py
@@ -26,10 +26,17 @@ matches = []
 for groups in phoneRegex.findall(text):
 	phoneNum = '-'.join([groups[1], groups[3], groups[5]])
 	if groups[8] != '':
-		matches.append(phoneNum)
+		phoneNum += ' x' + groups[8]
+	matches.append(phoneNum)
 
 for groups in emailRegex.findall(text):
 	matches.append(groups[0])
 
 
-# TODO: Copy results to the clipboard
+# Copy results to the clipboard
+if len(matches) > 0:
+	pyperclip.copy('\n'.join(matches))
+	print('Copied to clipboard:')
+	print('\n'.join(matches))
+else:
+	print('No phone numbers or email addresses found.')
