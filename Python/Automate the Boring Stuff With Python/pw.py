8d6ad202cee58157a70084005d6bd757ea4ecc56
Kedvall <kedvall123@gmail.com>
2016-07-01 13:01:36 -0500

    Finished password strength checking program

---
+++ Python/Automate the Boring Stuff With Python/pw.py
@@ -0,0 +1,8 @@
+#! python3
+# PasswordLockerProject.py - An insecure password locker program
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+			 'blog': 'VmALvQyKAxiCH5G8v01if1MLZF3sdt',
+			 'luggage': '12345'}
+
+			 
