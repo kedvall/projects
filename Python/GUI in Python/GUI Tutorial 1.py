f8b34958382a62610b139792100211cd62e4e424
Kedvall <kedvall123@gmail.com>
2016-07-12 13:11:10 -0500

    Started learning how to make GUI in Python

---
+++ Python/GUI in Python/GUI Tutorial 1.py
@@ -0,0 +1,10 @@
+#! python3
+# Tutorial from: https://www.youtube.com/watch?v=RJB1Ek2Ko_Y&list=PL6gx4Cwl9DGBwibXFtPtflztSNPGuIB_d
+
+from tkinter import *
+
+root = Tk()
+theLabel = Label(root, text="This is text.")
+theLabel.pack()
+
+root.mainloop()
