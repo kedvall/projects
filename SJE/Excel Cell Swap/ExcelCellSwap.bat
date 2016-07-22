18b23c5d2b4ca7730bf38ed043b2b48dec12d614
kedvall <kedvall123@gmail.com>
2016-07-22 08:47:41 -0500

    Renamed ExcelDataScraper to Excel Extractor. Also renamed Excel Data Mapper

--- SJE/Excel Cell Swap/ExcelCellSwap.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Launch options for ExcelCellSwap.py
-REM Makes it much easier to launch!
-REM Test path: C:\Users\%USERNAME%\Documents\dev\CompanySoftware\SJE\ExcelCellSwap.py
-
-echo "Launching from: C:\Users\%USERNAME%\Documents\Scripts\ExcelCellSwap.py"
-@py.exe "C:\Users\%USERNAME%\Documents\dev\CompanySoftware\SJE\ExcelCellSwap.py"
---
+++ SJE/Excel Extractor/ScriptBuilder.bat
@@ -0,0 +1,10 @@
+@echo off
+REM ^ is the little-known BAT file line continuation...
+
+pyinstaller --noconfirm ^
+			--log-level=WARN ^
+			--onefile ^
+			--windowed ^
+			--name "Excel Extractor"^
+			--icon="Excel Extractor.ico" ^
+			ExcelDataScraper.py
