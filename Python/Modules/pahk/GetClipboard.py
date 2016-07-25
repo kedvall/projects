841e1df087decf087655235a96a18cc690da4303
kedvall <kedvall123@gmail.com>
2016-07-25 16:58:15 -0500

    Added example for getting clipboard data from AHK to Python

---
+++ Python/Modules/pahk/GetClipboard.py
@@ -0,0 +1,19 @@
+#! python3
+from pahk import Interpreter
+from time import sleep
+    
+def get_clipboard():
+    ahk_interpreter = Interpreter()
+    ahk_script = '''#Persistent\nx = %clipboard%''' # Create a variable "x" which hold the clipboard data.
+    ahk_interpreter.execute_script(ahk_script)
+    sleep(0.5) #Let the thread warms up
+       
+    clipboard = ahk_interpreter.var_get('x') #Get the value of the variable x in the current running script
+        
+    ahk_interpreter.terminate() # Terminate the running script
+    del ahk_interpreter
+    
+    return clipboard 
+    
+print (get_clipboard())
+sleep(30)
