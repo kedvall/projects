ceac3c2e71c906a6517c77cf37b9e906e74eeebd
kedvall <kedvall123@gmail.com>
2016-07-28 00:47:55 -0500

    Fixed serial port not opening correctly

--- Programs To Deploy/RPiReceiver.py
+++ Programs To Deploy/RPiReceiver.py
@@ -8,18 +8,21 @@ import fcntl                # performs file control and I/O control on file desc
 import serial               # encapsulates the access for the serial port
   
 if __name__ == '__main__':
-    serial = serial.Serial()
-    serial.port = '/dev/ttyAMA0'
-    serial.baudrate = 112500
-    serial.timeout = 1
-    serial.writeTimeout = 1
-    serial.open()
+
+   serialConnection = serial.Serial(
+        port='/dev/ttyAMA0',
+        baudrate = 112500,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
  
     # make stdin a non-blocking file
     fcntl.fcntl(sys.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
  
     # post startup message to other XBee's and at stdout
-    serial.writelines("RPi #1 is up and running.\r\n")
+    serialConnection.writelines("RPi #1 is up and running.\r\n")
     print("RPi #1 is up and running.")
  
     print("Entering loop to read and print messages (Ctrl-C to abort)...")
@@ -27,18 +30,18 @@ if __name__ == '__main__':
     try:
         while True:
             # read a line from XBee and convert it from b'xxx\r\n' to xxx and print at stdout
-            line = serial.readline().decode('utf-8')
+            line = serialConnection.readline().decode('utf-8')
             if line:
                 print(line)
  
             # read data from the keyboard (i.e. stdin) and send via the XBee modem
             try:
                 line = sys.stdin.readline()
-                serial.writelines(line)
+                serialConnection.writelines(line)
             except IOError:
                 time.sleep(0.1)
                 continue
  
     except KeyboardInterrupt:
         print("\n*** Ctrl-C keyboard interrupt ***")
-        serial.writelines("RPi #1 is going down.\r\n")
+        serialConnection.writelines("RPi #1 is going down.\r\n")
